4e8816d6de6474bbd88d0c473163e4c334a13e7f
bewygs <benoit.wygas@protonmail.com>
2026-05-05 18:43:19 +0200

    TST: remove DOE test

--- tests/verification_tests/test_parser_verification.py
+++ tests/verification_tests/test_parser_verification.py
@@ -63,17 +63,6 @@ def test_parser_verification(input_file, parent_dir, file_prefix, tmp_path):
     # Get verification data directory
     verification_data_dir = get_verification_data_dir(Path(__file__).parent, parent_dir)
 
-    skip_files = {
-        "DOE202",
-        "DOE203",
-        "DOE204",
-        "DOE205",
-        "DOE206",
-    }  # under ventilated cases that takes too much time to run
-
-    if file_prefix in skip_files:
-        pytest.skip(f"Skipping slow test for {input_file}")
-
     # Parse the input file
     try:
         model = parse_cfast_file(input_file)
